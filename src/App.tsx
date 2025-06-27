8ff4c1d613958de57c120fe62af08d1176cebbc5
Navaneetha-Krishnan-MV <navaneethank45@gmail.com>
2025-06-27 21:38:51 +0530

    update jury comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import DeadLine from './Components/DeadLine.tsx'
 import AboutEvent from './Components/AboutEvent.tsx'
 import WhoCanJoin from './Components/WhoCanJoin.tsx'
-import Jury from './Components/Jury.tsx'
+// import Jury from './Components/Jury.tsx'
 import ContactSection from './Components/ContactSection.tsx'
 import SponserSection from './Components/SponserSection.tsx'
 import Front from "./Components/Front.tsx"
